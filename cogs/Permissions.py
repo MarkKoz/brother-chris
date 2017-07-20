a3debd30554e1ea3e72490d6cc3c461ad50437b8
MarkKoz <KozlovMark@gmail.com>
2017-07-20 01:52:53 -0700

    Permissions can now output left justified.

--- cogs/Permissions.py
+++ cogs/Permissions.py
@@ -10,6 +10,7 @@ class Permissions:
         self.bot = bot
         self.log = logging.getLogger("bot.cogs.Permissions")
         self.log.info("cogs.Permissions loaded successfully.")
+        self.config = Utils.loadConfig("Permissions")
 
     @commands.command(pass_context = True)
     async def perms(self,
@@ -32,21 +33,26 @@ class Permissions:
                  "Voice": self.getDict(discord.Permissions().voice())
                  } # type: Dict[str, Dict[str, bool]]
 
+        if self.config["justify"]:
+            width = self.getWidthMax(perms, self.config["padding"])
+        else:
+            width = 0
+
         embed = discord.Embed()
         embed.colour = discord.Colour(Utils.getRandomColour())
         embed.title = "Member Permissions"
         embed.description = f"Permissions for {user.mention} in {channel.mention}."
 
         embed.add_field(name = "General Permissions",
-                        value = self.getString(perms, "General"),
+                        value = self.getString(perms, "General", width),
                         inline = False)
 
         embed.add_field(name = "Text Permissions",
-                        value = self.getString(perms, "Text"),
+                        value = self.getString(perms, "Text", width),
                         inline = False)
 
         embed.add_field(name = "Voice Permissions",
-                        value = self.getString(perms, "Voice"),
+                        value = self.getString(perms, "Voice", width),
                         inline = False)
 
         await self.bot.send_message(destination = msg.channel, embed = embed)
@@ -66,15 +72,23 @@ class Permissions:
 
     @staticmethod
     def getString(perms: Dict[str, Dict[str, bool]],
-                  category: str) -> str:
+                  category: str,
+                  width: int) -> str:
+        print(f"Width: {width}")
         # Avoids appending newline to first item.
         key, value = perms[category].popitem()
-        string = f"{key} `{value}`"
+        # string = f"{key} `{value}`"
+        string = f"{key.ljust(width)} `{value}`"
 
         for key, value in perms[category].items():
-            string += f"\n{key} `{value}`"
+            string += f"\n{key.ljust(width)} `{value}`"
 
         return string
+
+    @staticmethod
+    def getWidthMax(d: Dict[any, Dict[str, any]], padding: int) -> int:
+        return max(len(key) for value in d.values()
+                   for key in value.keys()) + padding
 
 def setup(bot):
     bot.add_cog(Permissions(bot))
